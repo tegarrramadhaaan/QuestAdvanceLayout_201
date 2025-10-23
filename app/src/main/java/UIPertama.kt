dd73ac2d048667de02437eec405a2b3db7288467
tegarrramadhaaan <anakrama2000@gmail.com>
2025-10-23 09:33:12 +0700

    Initial commit

---
+++ app/src/main/java/UIPertama.kt
@@ -0,0 +1,21 @@
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun ActifitasPertama(modifier: Modifier){
+    Column(modifier = Modifier.padding(top=100.dp)
+        .fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(text = stringResource(id = R.string.pro
+
+
+
