0dc94e9b82cedf2f875b0e82cf24d870ef00d8b3
tegarrramadhaaan <anakrama2000@gmail.com>
2025-10-23 15:26:59 +0700

    buat composable utama ActivitasPertama dengan modifier opsional

--- app/src/main/java/UIPertama.kt
+++ app/src/main/java/UIPertama.kt
@@ -16,3 +16,6 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.myuipertama.R
+
+@Composable
+fun ActivitasPertama(modifier: Modifier = Modifier) {}
