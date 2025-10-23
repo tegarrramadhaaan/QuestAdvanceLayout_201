82c77ae7244a4a70e8de9d55e62d0acd4ebf3514
tegarrramadhaaan <anakrama2000@gmail.com>
2025-10-23 15:31:14 +0700

    tambahkan spacer antara judul dan kartu identitas

--- app/src/main/java/UIPertama.kt
+++ app/src/main/java/UIPertama.kt
@@ -37,5 +37,6 @@ fun ActivitasPertama(modifier: Modifier = Modifier) {
             text = stringResource(id = R.string.univ),
             fontSize = 22.sp
         )
-        }
+
+        Spacer(modifier = Modifier.height(25.dp))
 
